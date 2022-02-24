4be521f35c99f52655a316890fc51856f08834bc
Evgeniy <itsmeevg@gmail.com>
2022-02-24 11:46:35 +0300

    Initial commit

---
+++ layouts/main.jsx
@@ -0,0 +1,5 @@
+const MainLayout = () => (
+    <div>1</div>
+);
+
+export default MainLayout
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/line-clamp'),
+  ],
+}
