3386661011562a0db2415983e2a2572292cb9e64
Yan Alexandrov <yan@manu.team>
2022-03-06 15:47:55 +0300

    AMPLBLG-21 fix posts pagination

--- components/Posts/index.jsx
+++ components/Posts/index.jsx
@@ -1,38 +1,35 @@
 import PostCard from './PostCard';
 import Link from 'next/link';
 import {useRouter} from 'next/router';
-import {useEffect, useState} from "react";
+import {useEffect, useState} from 'react';
+import helpers from '../../helpers';
 
 const Posts = ({posts}) => {
   const [hotPost, setHotPost] = useState(null);
   const [postsList, setPostsList] = useState([]);
   const [loadMore, setLoadMore] = useState(true);
 
+  const postPerPage = helpers.getPostPerPage();
+
   const router = useRouter();
   const { tagID, page } = router.query;
 
-  const setQuery = (page, tagid) => {
-    let query = {
-      query: {
-        page: (page ? parseInt(page) + 1 : 2),
-      }
-    }
-    if (tagid) {
-      query = {...query, ...{
-          query: {
-            tagID: tagid
-          }
-        }};
-    }
-    return query;
-  }
-
   useEffect(() => {
     if (Array.isArray(posts) && posts.length) {
-      if (hotPost === null) {
+      if (
+        typeof tagID === 'undefined' && typeof page === 'undefined'   // is home page
+      ) {
+      //if ( ( typeof page === 'undefined' || parseInt(page) < 2 ) && ( hotPost === null || typeof tagID === 'undefined' ) ) {
         setHotPost(posts.shift());
       }
-      setPostsList(posts.splice(0, 3));
+
+      if (typeof page === 'undefined') {
+        setLoadMore(true);
+        setPostsList(posts.splice(0, postPerPage));
+      } else {
+        setPostsList([...postsList, ...posts.splice(0, postPerPage)]);
+      }
+
       if (!posts.length) {
         setLoadMore(false);
       }
@@ -58,21 +55,23 @@ const Posts = ({posts}) => {
               return <PostCard data={ post } key={ post.id }/>
             })
           }
-          <div className='p-card col-span-3 text-white text-center' key='subscribe'>insert the subscription form here</div>
+
+          <div className='p-card col-span-3 text-white text-center' key='subscribe'>
+            insert the subscription form here
+          </div>
+
           {
-            postsList.slice(3, 9).map((post) => {
+            postsList.slice(3, ( postPerPage * ( typeof page !== 'undefined' ? parseInt(page) + 1 : 2 ) )).map((post) => {
               return <PostCard data={ post } key={ post.id }/>
             })
           }
         </div>
 
-        { ( ( loadMore || typeof page === 'undefined' ) && postsList.length >= 3 ) &&
+        { ( ( loadMore || typeof page === 'undefined' ) && postsList.length >= postPerPage ) &&
         (
           <div className='pt-8 pb-8 text-center'>
-            <Link href={`?page=${page ? parseInt(page) + 1 : 2}` + (tagID ? `&tagID=${tagID}` : '')}>
-              <a
-                //onClick={() => router.push(setQuery(page, tagID))}
-                className='w-[118px] py-2 px-4 rounded text-white transition bg-dark-black-70 hover:bg-purple'>
+            <Link href={`?page=${page ? parseInt(page) + 1 : 2}` + (tagID ? `&tagID=${tagID}` : '')} scroll={false}>
+              <a className='w-[118px] py-2 px-4 rounded text-white transition bg-dark-black-70 hover:bg-purple'>
                 Load More
               </a>
             </Link>
