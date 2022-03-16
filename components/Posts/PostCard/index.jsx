e88fb63a99fb656c11fac33f5a19f27ee4ad4042
Yan Alexandrov <yan@manu.team>
2022-03-16 16:26:48 +0300

    AMPLBLG-41 resolve conflict with selector

--- components/Posts/PostCard/index.jsx
+++ components/Posts/PostCard/index.jsx
@@ -12,7 +12,7 @@ const PostCard = (props) => {
     return (
       <Link href={helpers.getPostSlug(post.title, post.id)} passHref>
         <a className="text-white transition-colors text-ellipsis display-block laptop:mb-[61px] group">
-          <div className={'overflow-hidden border border-solid border-dark-black-70 laptop:border-transparent laptop:border-transparent h-full hover:shadow-hover-post laptop:hover:border laptop:hover:border-dark-black-70 rounded-2xl pb-6 ' + (props.className !== 'undefined' ? props.className : '')} >
+          <div className={'animate-fadeIn overflow-hidden border border-solid border-dark-black-70 laptop:border-transparent transition-all duration-1000 laptop:border-transparent h-full hover:shadow-hover-post laptop:hover:border laptop:hover:border-dark-black-70 rounded-2xl pb-6 ' + (props.className !== 'undefined' ? props.className : '')} >
             <div className='overflow-hidden'>
               <Thumbnail
                 className='group-hover:scale-105 transition-all duration-[3000ms]'
