8ba9c0ab978118f2db5b32558a1acae444f995b2
kekaswork <kekaswork@gmail.com>
2022-03-24 12:15:15 +0100

    Avatar fixes

--- components/Posts/PostCard/Author/index.jsx
+++ components/Posts/PostCard/Author/index.jsx
@@ -6,12 +6,14 @@ const Author = (props) => {
   return (
     <div className={'flex items-center flex-row mt-6 ' + (props.className !== 'undefined' ? props.className : '')}>
       <span className={'w-8 h-8 rounded-full overflow-hidden relative ' + ( props.large ? 'laptop:w-[50px] laptop:h-[50px]' : '' )}>
-        <Image
-          src={props.avatar}
-          alt={props.name || 'Amplication Team'}
-          layout='fill'
-          objectPosition='center'
-        />
+        {props.avatar &&
+          <Image
+            src={props.avatar}
+            alt={props.name || 'Amplication Team'}
+            layout='fill'
+            objectPosition='center'
+          />
+        }
       </span>
       <div
         className={'flex flex-col ml-3 text-xs ' + ( props.large ? 'laptop:text-lg' : '' )}
