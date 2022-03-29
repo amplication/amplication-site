dd96183cdfda5708986983618ffd5a6159c3e038
kekaswork <kekaswork@gmail.com>
2022-03-29 13:39:19 +0200

    Team Page

---
+++ components/Sections/Team/HeroBlock/index.jsx
@@ -0,0 +1,38 @@
+import Image from "next/image";
+import teamVideoPlaceholder from '../../../../public/images/bg/team-video-placeholder.png';
+
+const HeroBlock = () => {
+  return (
+    <>
+      <section className="page-hero team-page background-transparent">
+        <div className="container-fluid">
+          <div className="row d-flex align-items-end align-items-lg-center">
+            <div className="col-12 col-lg-6">
+              <div className="hero-header">
+                <h1>
+                  We&nbsp;think big. We&nbsp;think&nbsp;different. We&nbsp;are
+                  Amplication.
+                </h1>
+              </div>
+            </div>
+            <div className="hero-bg col-12 col-lg-6">
+              <video autoPlay loop muted playsInline>
+                {/*<source*/}
+                {/*  src={'../../../../public/videos/amplication-team.mp4'}*/}
+                {/*  //src="./assets/videos/amplication-team.mp4"*/}
+                {/*  type="video/mp4"*/}
+                {/*/>*/}
+                <Image
+                  src={teamVideoPlaceholder}
+                  alt={''}
+                />
+              </video>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  )
+}
+
+export default HeroBlock
---
+++ pages/team.js
@@ -0,0 +1,39 @@
+import DocumentHead from "../components/Common/DocumentHead";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+import Background from "../components/Background";
+import HeroBlock from "../components/Sections/Team/HeroBlock";
+import TeamBlock from '../components/Sections/Team/TeamBlock';
+
+const Team = () => {
+  return (
+    <>
+      <DocumentHead
+        pageTitle="Amplication - Generate Node.js apps, just code what matters"
+        pageDescription="Amplication is an open‑source development tool. It helps you develop
+          quality Node.js applications without spending time on repetitive
+          coding tasks."
+        includeAssets={true}
+      />
+
+      <div className='page bg-dark-black-100 min-h-screen flex flex-col justify-start justify-items-stretch overflow-hidden pt-[60px] laptop:pt-[143px] bg-purple-dark relative'>
+        <Header/>
+
+        <main className="w-full font-poppins z-10 mb-12 laptop:mb-[100px] amplication-base circles-bg">
+
+          {/*Hero Block*/}
+          <HeroBlock />
+
+          {/*Team members*/}
+          <TeamBlock />
+
+        </main>
+
+        <Footer />
+        <Background />
+      </div>
+    </>
+  )
+}
+
+export default Team
