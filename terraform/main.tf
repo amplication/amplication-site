39fff441cbb15c748f9a2cc14799602419901411
Shahar <shaharb@sela.co.il>
2022-04-27 14:55:09 +0300

    try [id]

--- terraform/main.tf
+++ terraform/main.tf
@@ -69,7 +69,7 @@ resource "google_cloud_run_service_iam_member" "run_all_users" {
 }
 
 output "bobo" {
-  value = module.lb-http.backend_services.name
+  value = module.lb-http.backend_services[id]
   sensitive = true
 }
 
