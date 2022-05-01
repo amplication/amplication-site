b24155027dadc703b90195b35c6d3652ccde94b7
Shahar <shaharb@sela.co.il>
2022-05-01 21:18:17 +0300

    delete commented rows

--- terraform/main.tf
+++ terraform/main.tf
@@ -82,8 +82,6 @@ module "lb-http" {
   ssl                             = true
   managed_ssl_certificate_domains = [var.domain]
   create_url_map                  = false
-  # use_ssl_certificates            = true
-  # ssl_certificates                = ["new-cert-1234"]
   url_map                         = google_compute_url_map.urlmap.name
   backends = {
     default = {
