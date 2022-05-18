18773f9c5d25dd742d83e6a74a56619dca44aa8d
Pranay-Tej <s.pranayteja@gmail.com>
2022-05-18 16:30:21 +0530

    feat(site): add privacy-policy page

---
+++ pages/privacy-policy.js
@@ -0,0 +1,221 @@
+import DocumentHead from "../components/Common/DocumentHead"
+import Header from "../components/Header"
+import Footer from "../components/Footer"
+import Background from "../components/Background"
+
+const PrivacyPolicy = () => {
+  return (
+    <>
+      <DocumentHead
+        pageTitle="Amplication - Generate Node.js apps, just code what matters"
+        pageDescription="Amplication is an open-source development tool. It helps you develop
+          quality Node.js applications without spending time on repetitive
+          coding tasks."
+        includeAssets={true}
+      />
+
+      <div className="page min-h-screen flex flex-col justify-start justify-items-stretch overflow-hidden pt-[60px] laptop:pt-[110px] bg-purple-dark relative">
+        <Header />
+
+        <main className="w-full font-poppins z-10 mb-12 laptop:mb-[100px] amplication-base circles-bg">
+          <section class="pt-4 pt-md-11 home-background">
+            <div class="container mx-auto">
+              <div class="row align-items-center justify-content-center mb-4 bg-image-policy-header">
+                <h2 class="h2 text-center">Privacy Policy</h2>
+              </div>
+
+              <div class="p-4 row d-flex justify-content-between align-items-stretch policy-content-align">
+                <p>
+                  Amplication ("us", "we", or "our") operates the
+                  https://amplication.com website and
+                  https://app.amplication.com (the "Service").
+                </p>
+
+                <p>
+                  This page informs you of our policies regarding the
+                  collection, use, and disclosure of personal data when you use
+                  our Service and the choices you have associated with that
+                  data. We use your data to provide and improve the Service. By
+                  using the Service, you agree to the collection and use of
+                  information in accordance with this policy. Unless otherwise
+                  defined in this Privacy Policy, terms used in this Privacy
+                  Policy have the same meanings as in our Terms and Conditions.
+                </p>
+
+                <h3>Information Collection And Used</h3>
+                <p>
+                  We collect several different types of information for various
+                  purposes to provide and improve our Service to you.
+                </p>
+
+                <h3>Personal Data</h3>
+                <p>
+                  While using our Service, we may ask you to provide us with
+                  certain personally identifiable information that can be used
+                  to contact or identify you ("Personal Data"). Personally
+                  identifiable information may include, but is not limited to:
+                  Cookies and Usage Data
+                </p>
+
+                <h3>Usage Data</h3>
+                <p>
+                  We may also collect information that your browser sends
+                  whenever you visit our Service or when you access the Service
+                  by or through a mobile device ("Usage Data").
+                </p>
+
+                <p>
+                  This Usage Data may include information such as your
+                  computer's Internet Protocol address (e.g. IP address),
+                  browser type, browser version, the pages of our Service that
+                  you visit, the time and date of your visit, the time spent on
+                  those pages, unique device identifiers and other diagnostic
+                  data.
+                </p>
+
+                <p>
+                  When you access the Service by or through a mobile device,
+                  this Usage Data may include information such as the type of
+                  mobile device you use, your mobile device unique ID, the IP
+                  address of your mobile device, your mobile operating system,
+                  the type of mobile Internet browser you use, unique device
+                  identifiers and other diagnostic data.
+                </p>
+
+                <h3>Tracking & Cookies Data</h3>
+                <p>
+                  We use cookies and similar tracking technologies to track the
+                  activity on our Service and hold certain information. Cookies
+                  are files with small amount of data which may include an
+                  anonymous unique identifier. Cookies are sent to your browser
+                  from a website and stored on your device. Tracking
+                  technologies also used are beacons, tags, and scripts to
+                  collect and track information and to improve and analyze our
+                  Service.
+                </p>
+
+                <p>
+                  You can instruct your browser to refuse all cookies or to
+                  indicate when a cookie is being sent. However, if you do not
+                  accept cookies, you may not be able to use some portions of
+                  our Service. Examples of Cookies we use: Session Cookies. We
+                  use Session Cookies to operate our Service. Preference
+                  Cookies. We use Preference Cookies to remember your
+                  preferences and various settings. Security Cookies. We use
+                  Security Cookies for security purposes.
+                </p>
+
+                <h3>Use of Data</h3>
+                <p>
+                  Amplication uses the collected data for various purposes: To
+                  provide and maintain the Service. To notify you about changes
+                  to our Service. To allow you to participate in interactive
+                  features of our Service when you choose to do so. To provide
+                  customer care and support. To provide analysis or valuable
+                  information so that we can improve the Service. To monitor the
+                  usage of the Service. To detect, prevent and address technical
+                  issues.
+                </p>
+
+                <h3>Transfer Of Data</h3>
+                <p>
+                  Your information, including Personal Data, may be transferred
+                  to — and maintained on — computers located outside of your
+                  state, province, country or other governmental jurisdiction
+                  where the data protection laws may differ than those from your
+                  jurisdiction.
+                </p>
+                <p>
+                  Your consent to this Privacy Policy followed by your
+                  submission of such information represents your agreement to
+                  that transfer. Amplication will take all steps reasonably
+                  necessary to ensure that your data is treated securely and in
+                  accordance with this Privacy Policy and no transfer of your
+                  Personal Data will take place to an organization or a country
+                  unless there are adequate controls in place including the
+                  security of your data and other personal information.
+                </p>
+
+                <h3>Disclosure Of Data</h3>
+
+                <p>
+                  Legal Requirements: Amplication may disclose your Personal
+                  Data in the good faith belief that such action is necessary
+                  to: To comply with a legal obligation. To protect and defend
+                  the rights or property of Amplication. To prevent or
+                  investigate possible wrongdoing in connection with the
+                  Service. To protect the personal safety of users of the
+                  Service or the public. To protect against legal liability
+                </p>
+
+                <h3>Security Of Data</h3>
+                <p>
+                  The security of your data is important to us, but remember
+                  that no method of transmission over the Internet, or method of
+                  electronic storage is 100% secure. While we strive to use
+                  commercially acceptable means to protect your Personal Data,
+                  we cannot guarantee its absolute security.
+                </p>
+
+                <h3>Service Providers</h3>
+                <p>
+                  We may employ third party companies and individuals to
+                  facilitate our Service ("Service Providers"), to provide the
+                  Service on our behalf, to perform Service-related services or
+                  to assist us in analyzing how our Service is used. These third
+                  parties have access to your Personal Data only to perform
+                  these tasks on our behalf and are obligated not to disclose or
+                  use it for any other purpose.
+                </p>
+
+                <h3>Links To Other Sites</h3>
+                <p>
+                  Our Service may contain links to other sites that are not
+                  operated by us. If you click on a third party link, you will
+                  be directed to that third party's site. We strongly advise you
+                  to review the Privacy Policy of every site you visit. We have
+                  no control over and assume no responsibility for the content,
+                  privacy policies or practices of any third party sites or
+                  services.
+                </p>
+
+                <h3>Children's Privacy</h3>
+                <p>
+                  Our Service does not address anyone under the age of 18
+                  ("Children"). We do not knowingly collect personally
+                  identifiable information from anyone under the age of 18. If
+                  you are a parent or guardian and you are aware that your
+                  Children has provided us with Personal Data, please contact
+                  us. If we become aware that we have collected Personal Data
+                  from children without verification of parental consent, we
+                  take steps to remove that information from our servers.
+                </p>
+
+                <h3>Changes To This Privacy Policy</h3>
+                <p>
+                  We may update our Privacy Policy from time to time. We will
+                  notify you of any changes by posting the new Privacy Policy on
+                  this page. We will let you know via email and/or a prominent
+                  notice on our Service, prior to the change becoming effective
+                  and update the "effective date" at the top of this Privacy
+                  Policy.
+                </p>
+
+                <p>
+                  You are advised to review this Privacy Policy periodically for
+                  any changes. Changes to this Privacy Policy are effective when
+                  they are posted on this page.
+                </p>
+              </div>
+            </div>
+          </section>
+        </main>
+
+        <Footer />
+        <Background />
+      </div>
+    </>
+  )
+}
+
+export default PrivacyPolicy
