2cebb6fca0c8656bc26e48edeb1d1329b023cdc8
Dante-Nephilim <sakkara.ps@gmail.com>
2022-05-18 20:55:33 +0530

    feat: terms page

---
+++ pages/terms.js
@@ -0,0 +1,171 @@
+import DocumentHead from '../components/Common/DocumentHead';
+import Header from '../components/Header';
+import Footer from '../components/Footer';
+import Background from '../components/Background';
+
+const Terms = () => {
+  return (
+    <>
+      <DocumentHead
+        pageTitle="Amplication - Generate Node.js apps, just code what matters"
+        pageDescription="Amplication is an open-source development tool. It helps you develop
+          quality Node.js applications without spending time on repetitive
+          coding tasks."
+        includeAssets={true}
+      />
+
+      <div className="page min-h-screen flex flex-col justify-start justify-items-stretch overflow-hidden pt-[60px] laptop:pt-[110px] bg-purple-dark relative">
+        <Header />
+
+        <main className="w-full font-poppins z-10 mb-12 laptop:mb-[100px] amplication-base circles-bg">
+          <section className="pt-4 pt-md-11 home-background">
+            <div className="container mx-auto">
+              <div className="row align-items-center justify-content-center mb-4 bg-image-policy-header">
+                <h2 className="h2 text-center">Terms and Conditions</h2>
+              </div>
+
+              <div className="p-4 row d-flex justify-content-between align-items-stretch policy-content-align">
+                <p>Last updated: September 21, 2020</p>
+                <p>
+                  These Terms and Conditions (&quot;Terms&quot;, &quot;Terms and
+                  Conditions&quot;) govern your relationship with
+                  https://amplication.com website (the &quot;Service&quot;)
+                  operated by Amplication (&quot;us&quot;, &quot;we&quot;, or
+                  &quot;our&quot;).
+                  <br />
+                  Please read these Terms and Conditions carefully before using
+                  the Service. Your access to and use of the Service is
+                  conditioned on your acceptance of and compliance with these
+                  Terms. These Terms apply to all visitors, users and others who
+                  access or use the Service. By accessing or using the Service
+                  you agree to be bound by these Terms. If you disagree with any
+                  part of the terms then you may not access the Service.
+                </p>
+                <h3>Accounts</h3>
+                <p>
+                  When you create an account with us, you must provide us
+                  information that is accurate, complete, and current at all
+                  times. Failure to do so constitutes a breach of the Terms,
+                  which may result in immediate termination of your account on
+                  our Service. You are responsible for safeguarding the password
+                  that you use to access the Service and for any activities or
+                  actions under your password, whether your password is with our
+                  Service or a third-party service. You agree not to disclose
+                  your password to any third party. You must notify us
+                  immediately upon becoming aware of any breach of security or
+                  unauthorized use of your account.
+                </p>
+                <h3>Intellectual Property</h3>
+                <p>
+                  The Service and its original content, features and
+                  functionality are and will remain the exclusive property of
+                  Amplication and its licensors. The Service is protected by
+                  copyright, trademark, and other laws of both the Israel and
+                  foreign countries. Our trademarks and trade dress may not be
+                  used in connection with any product or service without the
+                  prior written consent of Amplication.
+                </p>
+                <h3>Links To Other Web Sites</h3>
+                <p>
+                  Our Service may contain links to third-party web sites or
+                  services that are not owned or controlled by Amplication.
+                  Amplication has no control over, and assumes no responsibility
+                  for, the content, privacy policies, or practices of any third
+                  party web sites or services. You further acknowledge and agree
+                  that Amplication shall not be responsible or liable, directly
+                  or indirectly, for any damage or loss caused or alleged to be
+                  caused by or in connection with use of or reliance on any such
+                  content, goods or services available on or through any such
+                  web sites or services. We strongly advise you to read the
+                  terms and conditions and privacy policies of any third-party
+                  web sites or services that you visit.
+                </p>
+                <h3>Termination</h3>
+                <p>
+                  We may terminate or suspend your account immediately, without
+                  prior notice or liability, for any reason whatsoever,
+                  including without limitation if you breach the Terms. Upon
+                  termination, your right to use the Service will immediately
+                  cease. If you wish to terminate your account, you may simply
+                  discontinue using the Service.
+                </p>
+                <h3>Indemnification</h3>
+                <p>
+                  You agree to defend, indemnify and hold harmless AMPLICATION
+                  and its licensee and licensors, and their employees,
+                  contractors, agents, officers and directors, from and against
+                  any and all claims, damages, obligations, losses, liabilities,
+                  costs or debt, and expenses (including but not limited to
+                  attorney&apos;s fees), resulting from or arising out of a)
+                  your use and access of the Website, or b) a breach of these
+                  Terms.
+                </p>
+                <h3>Limitation Of Liability</h3>
+                <p>
+                  In no event shall AMPLICATION, nor its directors, employees,
+                  partners, agents, suppliers, or affiliates, be liable for any
+                  indirect, incidental, special, consequential or punitive
+                  damages, including without limitation, loss of profits, data,
+                  use, goodwill, or other intangible losses, resulting from (i)
+                  your access to or use of or inability to access or use the
+                  Website; (ii) any conduct or content of any third party on the
+                  Website; (iii) any content obtained from the Website; and (iv)
+                  unauthorized access, use or alteration of your transmissions
+                  or content, whether based on warranty, contract, tort
+                  (including negligence) or any other legal theory, whether or
+                  not we have been informed of the possibility of such damage,
+                  and even if a remedy set forth herein is found to have failed
+                  of its essential purpose.
+                </p>
+                <h3>Disclaimer</h3>
+                <p>
+                  Your use of the Website is at your sole risk. The Website is
+                  provided on an "AS IS" and "AS AVAILABLE" basis. The Website
+                  is provided without warranties of any kind, whether express or
+                  implied, including, but not limited to, implied warranties of
+                  merchantability, fitness for a particular purpose,
+                  non-infringement or course of performance. AMPLICATION its
+                  subsidiaries, affiliates, and its licensors do not warrant
+                  that a) the Website will function uninterrupted, secure or
+                  available at any particular time or location; b) any errors or
+                  defects will be corrected; c) the Website is free of viruses
+                  or other harmful components; or d) the results of using the
+                  Website will meet your requirements.
+                </p>
+                <h3>Governing Law</h3>
+                <p>
+                  These Terms shall be governed and construed in accordance with
+                  the laws of Israel, without regard to its conflict of law
+                  provisions. Our failure to enforce any right or provision of
+                  these Terms will not be considered a waiver of those rights.
+                  If any provision of these Terms is held to be invalid or
+                  unenforceable by a court, the remaining provisions of these
+                  Terms will remain in effect. These Terms constitute the entire
+                  agreement between us regarding our Service, and supersede and
+                  replace any prior agreements we might have between us
+                  regarding the Service.
+                </p>
+                <h3>Changes</h3>
+                <p>
+                  We reserve the right, at our sole discretion, to modify or
+                  replace these Terms at any time. If a revision is material we
+                  will try to provide at least 30 days notice prior to any new
+                  terms taking effect. What constitutes a material change will
+                  be determined at our sole discretion. By continuing to access
+                  or use our Service after those revisions become effective, you
+                  agree to be bound by the revised terms. If you do not agree to
+                  the new terms, please stop using the Service.
+                </p>
+              </div>
+            </div>
+          </section>
+        </main>
+
+        <Footer />
+        <Background />
+      </div>
+    </>
+  );
+};
+
+export default Terms;
