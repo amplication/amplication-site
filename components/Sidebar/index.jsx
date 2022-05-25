fda1c61c4e03812229a637ee98f59c77e5893e1d
Yan Alexandrov <yan@manu.team>
2022-05-25 18:17:14 +0300

    AMPLBLG-53 UI fix

--- components/Sidebar/index.jsx
+++ components/Sidebar/index.jsx
@@ -3,11 +3,11 @@ import SubscribeForm from "../Common/SubscribeForm";
 
 const Sidebar = () => {
   return (
-    <div className='laptop:pt-32 medium:sticky medium:top-0 flex flex-col justify-start items-stretch w-full'>
+    <div className='laptop:pt-32 flex flex-col justify-start items-stretch w-full h-full'>
       <div className='w-full laptop:mb-16 order-2 laptop:order-1'>
         <SharingBlock />
       </div>
-      <div className='w-full order-1 laptop:order-2 mb-14 laptop:mb-0'>
+      <div className='medium:sticky medium:top-[7rem] w-full order-1 laptop:order-2 mb-14 laptop:mb-0'>
         <SubscribeForm
           isCompactView={true}
         />
