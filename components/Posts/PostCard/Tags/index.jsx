c95a22ad05c250133c396cb40c7e5168f31699eb
MuhammadJufri <sayyidmuhammad878@gmail.com>
2022-10-10 09:45:28 +0700

    Fix: Tags on Blog page are not rendered properly in Mobile view

--- components/Posts/PostCard/Tags/index.jsx
+++ components/Posts/PostCard/Tags/index.jsx
@@ -1,17 +1,17 @@
 import PropTypes from 'prop-types';
 
 const Tags = (props) => {
-  if ( props.list.length ) {
+  if (props.list.length) {
     return (
-      <div className={'mt-4 text-tiny text-white flex flex-row ' + (props.className !== 'undefined' ? props.className : '')}>
-        { props.list.map(tag => {
+      <div className={'mt-4 text-tiny text-white flex flex-row flex-wrap gap-y-3 ' + (props.className !== 'undefined' ? props.className : '')}>
+        {props.list.map(tag => {
           return (<span className='py-1 px-2 bg-light-blue rounded-full border border-solid border-lite mr-1.5 leading-tag font-medium' key={tag.id}>{tag.name}</span>)
         })}
       </div>
     )
   } else {
     return (
-      <div className={'mt-4 text-tiny text-white flex flex-row ' + (props.className !== 'undefined' ? props.className : '')}>
+      <div className={'mt-4 text-tiny text-white flex flex-row flex-wrap gap-col-3 ' + (props.className !== 'undefined' ? props.className : '')}>
         <span className='py-1 px-2 bg-light-blue rounded-full border border-solid border-lite mr-1.5 leading-tag'>Other</span>
       </div>
     )
