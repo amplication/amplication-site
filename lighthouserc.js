3d1deb19ce01960f5cada00df71dbadb038c6bf4
Michael Solati <mkslt04@gmail.com>
2022-10-16 22:44:24 -0700

    refactor(lhci): throw error

--- lighthouserc.js
+++ lighthouserc.js
@@ -15,10 +15,10 @@ module.exports = {
     },
     assert: {
       assertions: {
-        'categories:performance': ['warn', {minScore: 0.8}],
-        'categories:accessibility': ['warn', {minScore: 0.8}],
-        'categories:best-practices': ['warn', {minScore: 0.8}],
-        'categories:seo': ['warn', {minScore: 0.8}],
+        'categories:performance': ['error', {minScore: 0.8}],
+        'categories:accessibility': ['error', {minScore: 0.8}],
+        'categories:best-practices': ['error', {minScore: 0.8}],
+        'categories:seo': ['error', {minScore: 0.8}],
       },
     },
     upload: {
