dc15ccbf3fccdc54f5e9f06b917afe38de0ed3fa
Yan <yanalexandrov@gmail.com>
2022-11-14 18:12:55 +0300

    AMPLBLG-56 enteprise and about pages

--- pages/enterprise.js
+++ pages/enterprise.js
@@ -28,7 +28,6 @@ const Features = () => {
         'Using a community-driven plugin system, your team has full control over the flavor of the generated code, including security, privacy, standards, and best practices.<br />\n' +
         'Plugins can be used to control anything from the type of database to use, code conventions and format, authentication providers, connecting to CI/CD pipelines, monorepo management, build tools, and more.<br />\n' +
         'Your infra team can create custom plugins to keep the organization\'s current conventions, standards, and best practices and enforce usage across teams. You can easily update plugins and automatically push changes to dozen or hundreds of services in one click.',
-      buttons: [],
     },
     {
       image: enterprise3,
@@ -37,7 +36,6 @@ const Features = () => {
         'You build something unique, so use the creativity of your team to make a difference.<br />\n' +
         'The generated code is fully customizable. Your team keeps working with their favorite IDE to extend and build on top of the generated code. Anything can be customized, and Amplication will give you all the freedom to keep making changes without losing any of the custom code.<br />\n' +
         'Amplication will always create a Pull Request with the newly generated code, so your team has full control over what goes to production. No black boxing or vendor-locking. You own the code, as it should be, allowing you to build the next amazing product with no limits or obstacles.',
-      buttons: [],
     },
     {
       image: enterprise4,
@@ -46,7 +44,6 @@ const Features = () => {
         'Whether you build a single service or a microservices architecture, Amplication allows you to build at any scale.<br />\n' +
         'Your teams can create various services, manage microservices communication, use Kafka, connect to storage, or add an API Gateway. The code is consistent, secure, and reliable across all teams.<br />\n' +
         'Amplication can sync the generated code with a monorepo where each service goes to a different folder or sync with various repositories. You can manage dozens or hundreds of services with maximum consistency from a single source of truth and centralized management and visibility.',
-      buttons: [],
     },
     {
       image: enterprise5,
@@ -54,7 +51,6 @@ const Features = () => {
       content:
         'Whether you want to break the monolith, extend an existing product with new services, or start from scratch - Amplication got your back{end}.<br />\n' +
         'Amplication can connect to an existing monorepo and add new services alongside the existing ones, and the generated code can be extended to include the configuration, actions, and workflows to integrate with your CI/CD pipelines.',
-      buttons: [],
     },
   ];
 
@@ -75,20 +71,6 @@ const Features = () => {
                   <div className="col-sm-12 col-md-6 pr-3">
                     <div className="feature-title">{feature.title}</div>
                     <div className="feature-page-description text-base text-white" dangerouslySetInnerHTML={{__html: feature.content}}></div>
-                    {feature.buttons.length > 0 && (
-                      <div className="buttons-wrapper d-flex mb-4 align-items-center justify-content-start mt-4">
-                        {feature.buttons.map((button, index) => {
-                          return (
-                            <Link href={button.url ?? ''} passHref={true} key={index}>
-                              <a className={button.buttonClass ?? ''}>
-                                <span className={button.iconClass ?? ''}> </span>
-                                {button.title ?? ''}
-                              </a>
-                            </Link>
-                          );
-                        })}
-                      </div>
-                    )}
                   </div>
                   <div className="col-sm-12 col-md-6 feature-image" data-aos="fade-up-left">
                     {feature.image && (
