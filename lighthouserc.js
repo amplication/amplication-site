24ac1eec20451f35e824a732ff5c1c540a9001c2
Michael Solati <mkslt04@gmail.com>
2022-11-17 08:31:12 -0800

    feat(lhci): measure new pages

--- lighthouserc.js
+++ lighthouserc.js
@@ -3,13 +3,15 @@ module.exports = {
     collect: {
       url: [
         'http://localhost:3000/',
+        'http://localhost:3000/enterprise',
         'http://localhost:3000/features',
         'http://localhost:3000/pricing',
         'http://localhost:3000/community',
         'http://localhost:3000/team',
         'http://localhost:3000/blog',
-        'http://localhost:3000/tags/open-source',
         'http://localhost:3000/blog/the-open-source-development-platform-introducing-amplication',
+        'http://localhost:3000/tags/open-source',
+        'http://localhost:3000/about',
       ],
       startServerCommand: 'npm run start',
     },
