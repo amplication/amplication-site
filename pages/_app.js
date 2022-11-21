d0488bd51941d9a4b3d2e9fe3365c3d171d6bd38
Michael Solati <mkslt04@gmail.com>
2022-11-21 15:51:33 -0800

    chore: fixes

--- pages/_app.js
+++ pages/_app.js
@@ -86,27 +86,31 @@ function Amplication({Component, pageProps}) {
       </Head>
       <ApolloProvider client={client}>
         {getLayout(<Component {...pageProps} />)}
+        {/*Google Fonts*/}
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin={'true'}
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=DM+Mono:wght@400;500&family=Poppins:wght@400;500;600;700;800&family=Ubuntu+Mono&display=swap"
+          rel="stylesheet"
+        />
         {/*Facebook Pixel*/}
         <Script
           id={'facebook-pixel'}
-          strategy="afterInteractive"
+          strategy={'afterInteractive'}
           dangerouslySetInnerHTML={{
             __html: `
             !function(e,t,n,c,o,a,f){e.fbq||(o=e.fbq=function(){o.callMethod?o.callMethod.apply(o,arguments):o.queue.push(arguments)},e._fbq||(e._fbq=o),o.push=o,o.loaded=!0,o.version="2.0",o.queue=[],(a=t.createElement(n)).async=!0,a.src="https://connect.facebook.net/en_US/fbevents.js",(f=t.getElementsByTagName(n)[0]).parentNode.insertBefore(a,f))}(window,document,"script"),fbq("init","694076677979309"),fbq("track","PageView");
           `,
           }}
         />
-        {/*Google Fonts*/}
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
-        <link
-          href="https://fonts.googleapis.com/css2?family=DM+Mono:wght@400;500&family=Poppins:wght@400;500;600;700;800&family=Ubuntu+Mono&display=swap"
-          rel="stylesheet"
-        />
         {/*Hotjar Tracking Code*/}
         <Script
           id={'hotjar'}
-          strategy={'afterInteractive'}
+          strategy={'lazyOnload'}
           dangerouslySetInnerHTML={{
             __html: `
             !function(t,h,e,j,s,n){t.hj=t.hj||function(){(t.hj.q=t.hj.q||[]).push(arguments)},t._hjSettings={hjid:2379815,hjsv:6},s=h.getElementsByTagName("head")[0],(n=h.createElement("script")).async=1,n.src="https://static.hotjar.com/c/hotjar-"+t._hjSettings.hjid+".js?sv="+t._hjSettings.hjsv,s.appendChild(n)}(window,document);
@@ -115,7 +119,7 @@ function Amplication({Component, pageProps}) {
         />
         <Script
           id={'intercomSettings'}
-          strategy="afterInteractive"
+          strategy={'lazyOnload'}
           dangerouslySetInnerHTML={{
             __html: `
             window.intercomSettings = {
@@ -126,7 +130,7 @@ function Amplication({Component, pageProps}) {
         />
         <Script
           id={'intercom'}
-          strategy="afterInteractive"
+          strategy={'lazyOnload'}
           dangerouslySetInnerHTML={{
             __html: `
             (function(){var w=window;var ic=w.Intercom;if(typeof ic==="function"){ic('reattach_activator');ic('update',w.intercomSettings);}else{var d=document;var i=function(){i.c(arguments);};i.q=[];i.c=function(args){i.q.push(args);};w.Intercom=i;var l=function(){var s=d.createElement('script');s.type='text/javascript';s.async=true;s.src='https://widget.intercom.io/widget/rtlni1hg';var x=d.getElementsByTagName('script')[0];x.parentNode.insertBefore(s,x);};if(w.attachEvent){w.attachEvent('onload',l);}else{w.addEventListener('load',l,false);}}})();
@@ -135,7 +139,7 @@ function Amplication({Component, pageProps}) {
         />
         <Script
           id={'segment'}
-          strategy="afterInteractive"
+          strategy={'afterInteractive'}
           dangerouslySetInnerHTML={{
             __html: `
             !function(){var analytics=window.analytics=window.analytics||[];if(!analytics.initialize)if(analytics.invoked)window.console&&console.error&&console.error("Segment snippet included twice.");else{analytics.invoked=!0;analytics.methods=["trackSubmit","trackClick","trackLink","trackForm","pageview","identify","reset","group","track","ready","alias","debug","page","once","off","on","addSourceMiddleware","addIntegrationMiddleware","setAnonymousId","addDestinationMiddleware"];analytics.factory=function(e){return function(){var t=Array.prototype.slice.call(arguments);t.unshift(e);analytics.push(t);return analytics}};for(var e=0;e<analytics.methods.length;e++){var key=analytics.methods[e];analytics[key]=analytics.factory(key)}analytics.load=function(key,e){var t=document.createElement("script");t.type="text/javascript";t.async=!0;t.src="https://cdn.segment.com/analytics.js/v1/" + key + "/analytics.min.js";var n=document.getElementsByTagName("script")[0];n.parentNode.insertBefore(t,n);analytics._loadOptions=e};analytics._writeKey="GpXLWZ8HnAhUuUfZBs6bE5IE87yHmBtu";analytics.SNIPPET_VERSION="4.13.2";
@@ -150,13 +154,13 @@ function Amplication({Component, pageProps}) {
         {/* HubSpot */}
         <Script
           id={'hs-script-loader'}
-          strategy="afterInteractive"
+          strategy={'afterInteractive'}
           src="//js-eu1.hs-scripts.com/25691669.js"
         />
         {/* Google Tag Manager */}
         <Script
           id={'gtm'}
-          strategy="afterInteractive"
+          strategy={'afterInteractive'}
           dangerouslySetInnerHTML={{
             __html: `
             (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
