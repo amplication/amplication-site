629605ee0b20c5429c3df98c72ef87e67bed0851
Yan <yanalexandrov@gmail.com>
2022-12-07 18:21:02 +0300

    AMPLBLG-55 UI fixes

--- components/Header/Menu/index.jsx
+++ components/Header/Menu/index.jsx
@@ -1,7 +1,7 @@
 import Link from 'next/link';
 import Button from '../../Common/Button';
 import {useRouter} from 'next/router';
-import {useState} from 'react';
+import {useState,useEffect} from 'react';
 import useWindowSize from '../../../utils/useWindowSize';
 
 const Menu = () => {
