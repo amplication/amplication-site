86d3fd475af851a229eb8d670e138d24a3e36a7a
Michael Solati <mkslt04@gmail.com>
2023-02-14 08:55:35 -0800

    fix(toc): fix scroll and appearance

--- components/Posts/TOC/index.jsx
+++ components/Posts/TOC/index.jsx
@@ -5,6 +5,16 @@ import styles from './style.module.css';
  * @param {{headings: Heading[], open: boolean}} props
  */
 const TOC = props => {
+  const smoothScroll = e => {
+    e.preventDefault();
+    const id = e.target.getAttribute('data-id');
+    const header = document.getElementById(id);
+    const offset =
+      header.getBoundingClientRect().top + window.pageYOffset - 150;
+    history.replaceState(undefined, undefined, `#${id}`);
+    window.scrollTo({top: offset, behavior: 'smooth'});
+  };
+
   return props.headings.length === 0 ? (
     ''
   ) : (
@@ -15,12 +25,28 @@ const TOC = props => {
       <ul className={styles.toc}>
         {props.headings.map((h, i) => (
           <li key={i} className={styles.toc__element}>
-            {h.title ? <a href={'#' + h.id}>{h.title}</a> : ''}
+            {h.title ? (
+              <a
+                onClick={e => smoothScroll(e)}
+                data-id={h.id}
+                href={'#' + h.id}
+              >
+                {h.title}
+              </a>
+            ) : (
+              ''
+            )}
             {h.children.length > 0 ? (
               <ul className={styles.toc}>
                 {h.children.map((c, i) => (
                   <li key={i} className={styles.toc__element}>
-                    <a href={'#' + c.id}>{c.title}</a>
+                    <a
+                      onClick={e => smoothScroll(e)}
+                      data-id={c.id}
+                      href={'#' + c.id}
+                    >
+                      {c.title}
+                    </a>
                   </li>
                 ))}
               </ul>
