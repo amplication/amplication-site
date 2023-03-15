7557013a51778983701a2196baf90d54b6e684e5
Yan <yanalexandrov@gmail.com>
2023-03-15 15:21:37 +0300

    AMPLBLG-62 add webinar bannner

--- components/Header/index.jsx
+++ components/Header/index.jsx
@@ -1,13 +1,13 @@
-import TopBar from './TopBar';
+import WebinarBar from './WebinarBar';
 import MainBar from './MainBar';
 import BottomBar from './BottomBar';
 
 const Header = () => {
   return (
     <>
-      <header className="z-20 font-poppins laptop:fixed laptop:w-full laptop:top-0 h-[150px]">
+      <header className="z-20 font-poppins laptop:fixed laptop:w-full laptop:top-0 h-[150px] max-laptop:h-[225px]">
         <div className="fixed w-full">
-          <TopBar />
+          <WebinarBar />
           <MainBar />
           <BottomBar />
         </div>
