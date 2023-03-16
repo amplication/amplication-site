d99f7d74b78c8cd458cd2a49c2ce6b5b11de39cd
yuval-hazaz <hazaz.yuval@gmail.com>
2023-03-16 21:53:53 +0200

    update h1

--- pages/privacy-policy.js
+++ pages/privacy-policy.js
@@ -15,7 +15,7 @@ const PrivacyPolicy = () => {
         <section className="pt-4 pt-md-11 home-background">
           <div className="container mx-auto">
             <div className="row align-items-center justify-content-center mb-4 bg-image-policy-header">
-              <h2 className="h2 text-center">Privacy Policy</h2>
+              <h1 className="h2 text-center">Amplication Privacy Policy</h1>
             </div>
 
             <div className="p-4 row d-flex justify-content-between align-items-stretch policy-content-align">
