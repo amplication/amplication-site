35d95c7358309b15c531c6fb30c0fbbdddff5eeb
Yan <yanalexandrov@gmail.com>
2023-03-29 14:41:33 +0300

    AMPLBLG-63 add testimonials on contact us page

--- helpers/index.js
+++ helpers/index.js
@@ -174,6 +174,15 @@ const helpers = {
 
     return headings;
   },
+  getInitials: string => {
+    let names = string.split(' '),
+      initials = names[0].substring(0, 1).toUpperCase();
+
+    if (names.length > 1) {
+      initials += names[names.length - 1].substring(0, 1).toUpperCase();
+    }
+    return initials;
+  },
 };
 
 export default helpers;
