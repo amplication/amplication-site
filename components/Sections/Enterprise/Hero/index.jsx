479e31a7590517872dcc26d779b8f0b03787ecfc
Yan <yanalexandrov@gmail.com>
2023-04-30 15:15:22 +0300

    AMPLBLG-64 update UI

--- components/Sections/Enterprise/Hero/index.jsx
+++ components/Sections/Enterprise/Hero/index.jsx
@@ -3,7 +3,7 @@ import Image from 'next/image';
 import Link from 'next/link';
 import AppDashboard from '../../../../public/images/enterprise/app-dashboard.svg';
 import * as analytics from '../../../../lib/analytics';
-import VideoWatch from "../../../../public/images/video-watch.png";
+import VideoWatch from '../../../../public/images/video-watch.png';
 
 const Hero = () => {
   const handleStartNowClick = useCallback(() => {
