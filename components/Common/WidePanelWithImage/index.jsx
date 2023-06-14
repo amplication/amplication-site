2591e000a172cab7cd0e805c775a0aa50dd9f4a5
Yuval Hazaz <yuval@amplication.com>
2023-06-14 11:17:37 +0300

    feat: create landing pge for DB import beta program (#377)
    * update url
    
    * update team page
    
    * feat:db-inport-beta page
    
    * feat:db-inport-beta page
    
    * feat:db-inport-beta page
    
    * fix linting errors
    
    * fix formatting
    
    * update param name

---
+++ components/Common/WidePanelWithImage/index.jsx
@@ -0,0 +1,96 @@
+import Image from 'next/image';
+import PropTypes from 'prop-types';
+import Link from 'next/link';
+
+const WidePanelWithImage = ({
+  image,
+  title,
+  customClasses,
+  primaryBtn,
+  secondaryBtn,
+  content,
+  children,
+}) => {
+  return (
+    <section
+      className={
+        'features-page-feature !py-10 desktop:!py-24 ' + (customClasses ?? '')
+      }
+    >
+      <div className="w-full max-w-container m-container p-container laptop:max-w-container-desktop laptop:m-container-desktop laptop:p-container-desktop">
+        <div
+          className={
+            'row d-flex justify-content-between align-items-center feature-page-card py-0 laptop:py-6'
+          }
+        >
+          <div className={'col-sm-12 col-md-6 pb-0 desktop:pt-8 pr-3'}>
+            <div className="!text-[24px] desktop:!text-[32px] !leading-[36px] desktop:!leading-[48px] font-semibold pb-2">
+              {title}
+            </div>
+            <div
+              className="text-sm desktop:text-lg text-white desktop:!leading-[27px] max-w-[600px]"
+              dangerouslySetInnerHTML={{__html: content}}
+            ></div>
+            {children}
+            <div className="buttons-wrapper d-flex tablet:align-items-center justify-content-start mt-4">
+              {primaryBtn && (
+                <Link href={primaryBtn.href} passHref={true}>
+                  <a className="btn btn-primary btn-sm !h-[34px] mb-2 mr-6 w-full !font-normal !text-sm !max-w-[140px]">
+                    {primaryBtn.title}
+                  </a>
+                </Link>
+              )}
+              {secondaryBtn && (
+                <Link href={secondaryBtn.href ?? ''} passHref={true}>
+                  <a className="btn btn-outline-light btn-sm !h-[34px] mb-2 laptop:mr-2 w-full !font-normal !text-sm !max-w-[140px]">
+                    {secondaryBtn.title}
+                  </a>
+                </Link>
+              )}
+            </div>
+          </div>
+          <div
+            className="col-sm-12 col-md-6 desktop:left-4 desktop:top-6 feature-image"
+            data-aos="fade-up-left"
+          >
+            {image && (
+              <Image
+                className="max-w-[710px] !visible"
+                src={image}
+                alt={title}
+                objectFit={'cover'}
+              />
+            )}
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+WidePanelWithImage.propTypes = {
+  children: PropTypes.any,
+  image: PropTypes.string,
+  title: PropTypes.string,
+  customClasses: PropTypes.string,
+  primaryBtn: {
+    title: PropTypes.string,
+    href: PropTypes.string,
+  },
+  secondaryBtn: {
+    title: PropTypes.string,
+    href: PropTypes.string,
+  },
+  content: PropTypes.string,
+};
+
+WidePanelWithImage.defaultProps = {
+  image: '',
+  title: '',
+  customClasses: '',
+  primaryBtn: undefined,
+  secondaryBtn: undefined,
+  content: '',
+};
+
+export default WidePanelWithImage;
--- components/Sections/Enterprise/StartNow/index.jsx
+++ components/Sections/Enterprise/StartNow/index.jsx
@@ -1,10 +1,16 @@
 import Link from 'next/link';
 import PropTypes from 'prop-types';
 
-const StartNow = ({title, description, linkPrimary, linkSecondary}) => {
+const StartNow = ({
+  title,
+  description,
+  linkPrimary,
+  linkSecondary,
+  backgroundClass,
+}) => {
   return (
     <>
-      <section className="background-transparent w-full max-w-container m-container p-container laptop:max-w-container-desktop laptop:m-container-desktop laptop:p-container-desktop !py-[100px]">
+      <section className={'!pt-5 !pb-4 laptop:!py-20 ' + backgroundClass}>
         <h2 className="mx-auto text-[32px] !font-semibold leading-[48px] tracking-normal text-center max-w-[960px]">
           {title}
         </h2>
@@ -18,7 +24,7 @@ const StartNow = ({title, description, linkPrimary, linkSecondary}) => {
           {linkPrimary.href && (
             <Link href={linkPrimary.href} passHref={true}>
               <a
-                target="_blank"
+                target={linkPrimary.target || '_blank'}
                 className="btn btn-primary btn-sm !text-sm !h-10 mr-4 w-[120px] !px-0 !h-[34px] !font-normal"
                 rel="noreferrer"
               >
@@ -29,7 +35,7 @@ const StartNow = ({title, description, linkPrimary, linkSecondary}) => {
           {linkSecondary.href && (
             <Link href={linkSecondary.href} passHref={true}>
               <a
-                target="_blank"
+                target={linkSecondary.target || '_blank'}
                 className="d-flex justify-content-center align-items-center outline-button !text-sm text-white !h-10 mr-2 w-[120px] rounded !px-0 !h-[34px] !font-normal !bg-light-blue hover:!bg-dark-black-70"
                 rel="noreferrer"
               >
@@ -48,18 +54,23 @@ StartNow.propTypes = {
   description: PropTypes.string,
   linkPrimary: PropTypes.object,
   linkSecondary: PropTypes.object,
+  backgroundClass: PropTypes.string,
+  target: PropTypes.string,
 };
 
 StartNow.defaultProps = {
   title: '',
   description: '',
+  backgroundClass: 'background-transparent',
   linkPrimary: {
     title: '',
     href: '',
+    target: '',
   },
   linkSecondary: {
     title: '',
     href: '',
+    target: '',
   },
 };
 
