d43690c14b92386812ad3f974cdb471c42c75c8d
yuval-hazaz <hazaz.yuval@gmail.com>
2023-09-06 08:25:46 +0300

    update plugins page

--- pages/plugins.js
+++ pages/plugins.js
@@ -5,33 +5,63 @@ import Plugin from "../components/Plugins/Plugin";
 import helpers from '../helpers';
 import { MainLayout } from '../layouts';
 import client from '../services/plugin-api';
+import Link from 'next/link';
 
 const Plugins = ({ plugins }) => {
 
   return (
     <>
       <NextSeo
-        title="Plugins: Node.js Insights for Developers & Enterprises | Amplication"
+        title="Plugins: Extend the generate code with plugins | Amplication"
         description="Boost your knowledge and step up your game with top stories on backend development, Node.js and open-source from the Amplication team."
       />
+      <main className="w-full font-poppins z-10 mt-12 mb-12 laptop:mb-[100px] ">
+        <section className={'background-transparent amplication-base mb-12'}>
+          <div className="w-full max-w-container m-container p-container laptop:max-w-container-desktop laptop:m-container-desktop laptop:p-container-desktop">
+            <div className="row align-items-center justify-content-center mb-4">
+              <h1
 
-      <main className="w-full font-poppins z-10 mb-12 laptop:mb-[100px] laptop:pt-10">
-        {Array.isArray(plugins) && !!plugins.length && (<div>
-          <div className={
-            'w-full max-w-container m-container p-container laptop:max-w-container-desktop-blog laptop:m-container-desktop laptop:p-container-desktop pt-0 laptop:pt-0 laptop:pb-0 grid grid-cols-1 tablet:grid-cols-2 laptop:grid-cols-3 gap-7.5 laptop:gap-x-7.5 laptop:gap-y-[61px]'
-          }>
-            {plugins.map((plugin) => (
-              <Plugin key={plugin.pluginId} plugin={plugin} />
+                className="mx-auto h1 !font-semibold  tracking-normal text-center max-w-[960px]"
+              >Find a Plugin</h1>
 
-            ))}
+            </div>
+            <div >
+              <h2 className="h3 text-center max-w-[960px] mx-auto text-gray">
+                Extend and customize your services by using plugins for various technologies and integrations.
+              </h2>
+            </div>
           </div>
-        </div>)}
-        {(!Array.isArray(plugins) || !plugins.length) && (
-          <div className="w-full max-w-container m-container p-container laptop:max-w-container-desktop laptop:m-container-desktop laptop:p-container-desktop py-12 text-white text-center !pb-12">
-            plugins not found
+
+        </section>
+        <div className={'w-full max-w-container m-container p-container laptop:max-w-container-desktop-blog laptop:m-container-desktop laptop:p-container-desktop  laptop:pb-0   '}>
+          <div className='mb-10'>
+            <Link href="https://docs.amplication.com/plugins/overview/">
+              <a className="text-white text-xs font-poppins font-medium py-1 px-2 inline-block">
+                Learn more about plugins
+              </a>
+            </Link>
+            <Link href="https://docs.amplication.com/plugins/how-to-create-plugin/">
+              <a className="text-white text-xs font-poppins font-medium py-1 px-2 inline-block">
+                Create your own plugin
+              </a>
+            </Link>
           </div>
-        )}
-      </main>
+        </div>
+
+
+
+        <div className={'mt-4 w-full max-w-container m-container p-container laptop:max-w-container-desktop-blog laptop:m-container-desktop laptop:p-container-desktop pt-0 laptop:pt-0 laptop:pb-0 grid grid-cols-1 tablet:grid-cols-2 laptop:grid-cols-3 gap-7.5 laptop:gap-x-7.5 laptop:gap-y-[61px]'}>
+          {plugins.map((plugin) => (
+            <Plugin key={plugin.pluginId} plugin={plugin} />
+          ))}
+          {(!Array.isArray(plugins) || !plugins.length) && (
+            <div>
+              plugins not found
+            </div>
+          )}
+        </div>
+
+      </main >
     </>
   );
 };
