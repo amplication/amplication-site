1242dadb55aa89a5694cfce880e7b6b971b1f039
yuval-hazaz <hazaz.yuval@gmail.com>
2023-11-01 19:27:15 +0200

    style: align pricing page

--- components/Sections/Pricing/HeroBlock/index.jsx
+++ components/Sections/Pricing/HeroBlock/index.jsx
@@ -9,7 +9,7 @@ const HeroBlock = () => {
         <div className="w-full max-w-container m-container p-container laptop:max-w-container-desktop laptop:m-container-desktop laptop:p-container-desktop">
           <div className="row d-flex align-items-end align-items-md-center">
             <div className="col-12 ">
-              <h1 className="main-title pricing mb-5 text-center">
+              <h1 className="main-title pricing mt-5 text-center">
                 Accelerate your backend development.
                 <div style={mystyle}>Start FREE </div>
               </h1>
