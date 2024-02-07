6bad1fcf2423d100f3fb59cc2891c317477f2e74
yuval-hazaz <hazaz.yuval@gmail.com>
2024-02-07 11:21:19 +0200

    style:plugin list

--- components/Plugins/plugin-card.jsx
+++ components/Plugins/plugin-card.jsx
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 import { useCallback } from 'react';
 import helpers from '../../helpers';
 import * as analytics from '../../lib/analytics';
-import Button from '../Common/Button';
+import OutlineButton from '../Common/Button/button-outline';
 import PluginLogo from './plugin-logo';
 
 const Plugin = ({ plugin }) => {
@@ -26,8 +26,10 @@ const Plugin = ({ plugin }) => {
     });
   }, [plugin.pluginId]);
 
+  const pluginPageUrl = helpers.getPluginSlug(plugin.pluginId);
+
   return (
-    <Link href={helpers.getPluginSlug(plugin.pluginId)} passHref>
+    <Link href={pluginPageUrl} passHref>
       <a
         className="text-white transition-colors text-ellipsis display-block laptop:mb-0 group"
         onClick={handleClick}
@@ -35,33 +37,28 @@ const Plugin = ({ plugin }) => {
         <div
           key={plugin.pluginId}
           className={
-            'flex flex-col justify-between items-start overflow-hidden border border-solid border-dark-black-70 bg-purple-light  h-full hover:shadow-hover-post rounded-lg p-4 min-h-[200px] '
+            'flex flex-col justify-between items-start overflow-hidden border border-solid border-dark-black-70 bg-dark-black-100  h-full hover:shadow-hover-post rounded-lg p-4 min-h-[200px] '
           }
         >
           <div className={'flex flex-row justify-start items-center  '}>
-            <PluginLogo plugin={plugin} />
+            <PluginLogo plugin={plugin} size={62} />
             <div>
               <div className="text-base">{plugin.name}</div>
             </div>
           </div>
-          <span className="text-xs text-gray py-4">{plugin.description}</span>
-          <div className="flex flex-row justify-between items-center mt-auto pt-4 w-full border-t-1 border-t-dark-black-70 ">
+          <span className="text-xs text-gray py-4 max-h-[64px] overflow-hidden">
+            {plugin.description}
+          </span>
+          <div className="flex flex-row justify-end items-center mt-auto pt-8 w-full ">
             <span className="text-xs text-white">
-              <a href={plugin.github} target="github_plugin">
-                View Source
-              </a>
-            </span>
-            <span className="text-xs text-white">
-              <Button
-                text="Add Plugin"
-                backgroundColor="purpleBright"
-                hoverBackgroundColor="purpleBrightHover"
+              <OutlineButton
+                text="Learn more"
                 isLink={true}
                 onClick={() => {
                   handleStartNowClick(plugin.pluginId);
                 }}
-                href={`https://app.amplication.com/login?plugin-id=${plugin.pluginId}`}
-                className=" whitespace-nowrap !px-2 !py-2"
+                href={pluginPageUrl}
+                className="whitespace-nowrap !px-2 !py-2"
                 delayLinkMs={300} //add 300ms to be able to send the event
               />
             </span>
