7f2521e9192da3dcf44745bd997e027a90440525
yuval-hazaz <hazaz.yuval@gmail.com>
2024-10-07 15:50:32 +0300

    hp:platform

--- pages/index.js
+++ pages/index.js
@@ -6,9 +6,43 @@ import HeroBlock from '../components/Sections/Enterprise/HeroBlock';
 import Slider from '../components/Sections/Enterprise/Slider';
 import GetList from '../components/Sections/MainPage/GetList';
 import { MainLayout } from '../layouts';
+import CardsList from '../components/Common/CardsList';
+import IconDev from '../public/images/icons/icon-development.svg';
+import IconDeploy from '../public/images/icons/icon-deploy.svg';
+import IconCloud from '../public/images/icons/icon-cloud.svg';
+import IconCollab from '../public/images/icons/icon-10.svg';
+
+
 
 import TestimonialsSlider from '../components/Sections/Enterprise/Testimonials/testimonials-slider';
-import Steps from '../components/Sections/MainPage/Steps';
+import Steps from '../components/Sections/MainPage/StepsPlatform';
+
+const cards = [
+  {
+    title: 'Flexible yet standardized',
+    subTitle:
+      "Create tailored live templates with  your standards, ensuring consistency across development teams.",
+    icon: IconDev,
+  },
+  {
+    title: 'Fast and simple ',
+    subTitle:
+      "Build is hours not days. Automate foundational coding tasks so developers can focus on building new features. ",
+    icon: IconDeploy,
+  },
+  {
+    title: 'Always up-to-date',
+    subTitle: "Your applications stay current with automatic updates, aligning with the latest standards and technologies.",
+    icon: IconCloud,
+  },
+  {
+    title: 'Seamless Collaboration',
+    subTitle: "A unified platform where both platform engineers and developers collaborate seamlessly.",
+    icon: IconCollab,
+  },
+
+
+];
 
 const Home = () => {
   return (
@@ -38,20 +72,27 @@ const Home = () => {
         <PageSection light className={'py-5'} innerClassName={"flex-grow"}>
           <Slider />
         </PageSection>
-        <PageSection className={"py-10"} alternate>
+        {/* <PageSection className={"py-10"} alternate>
           <Facts />
-        </PageSection>
+        </PageSection> */}
         <PageSection className={'py-5'} >
           <Steps />
         </PageSection>
-        <PageSection  >
+        <PageSection alternate className={'py-5'}>
           <h2 className="my-12 mx-auto text-[32px] !font-semibold leading-[48px] tracking-normal text-center max-w-[960px]">
             What users are saying about Amplication?
           </h2>
           <TestimonialsSlider useSlider={false} totalItems={3} wrapperClassName={"laptop:!grid-cols-3"} />
         </PageSection>
-        <PageSection alternate className={"!py-20"} >
-          <GetList />
+        <PageSection className={"!py-20"} >
+          {/* <GetList /> */}
+          <h2 className="my-12 mx-auto text-[32px] !font-semibold leading-[48px] tracking-normal text-center max-w-[960px]">
+            Why Amplication
+          </h2>
+          <CardsList
+            cards={cards}
+            customClasses={'tablet:!grid-cols-1 laptop:!grid-cols-4'}
+          />
         </PageSection>
         <PageSection >
           <Soc2Banner />
