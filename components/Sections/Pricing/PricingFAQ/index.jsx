bbba737c132a73b91d59061070b2dff112a37257
yuval-hazaz <hazaz.yuval@gmail.com>
2024-10-29 19:34:20 +0200

    update pricing page and faq

--- components/Sections/Pricing/PricingFAQ/index.jsx
+++ components/Sections/Pricing/PricingFAQ/index.jsx
@@ -59,23 +59,11 @@ const PricingFAQ = () => {
                     <br />
                     <strong>Free Plan:</strong>
                     <br />
-                    Ideal for individual developers working on small-scale
-                    applications. This plan includes core features like Node.js
-                    support, database integration, roles & permissions, GraphQL
-                    & REST APIs, and unlimited code generation builds. It&apos;s
-                    perfect for getting started without any cost.
-                  </p>
-                </li>
-                <li>
-                  <p>
-                    <br />
-                    <strong>Essential Plan:</strong>
-                    <br />
-                    This plan is best for small teams building collaborative
-                    projects. It includes enhanced capabilities such as support
-                    for both Node.js and .NET, more Jovu requests, multiple
-                    services, custom actions support, and additional team
-                    members.
+                    Ideal for individual looking to harness the power of our
+                    code generation engine. This plan includes core features
+                    like Node.js support, database integration, roles &
+                    permissions, GraphQL & REST APIs, and unlimited code
+                    generation builds.
                   </p>
                 </li>
                 <li>
@@ -83,17 +71,16 @@ const PricingFAQ = () => {
                     <br />
                     <strong>Enterprise Plan:</strong>
                     <br />
-                    For larger organizations with more complex needs, the
-                    Enterprise plan is the best fit. It includes all the
-                    features of the Essential plan plus unlimited Jovu requests,
-                    projects, services, and team members. Additionally, it
-                    offers advanced security features like SSO and 2FA,
-                    comprehensive Git sync options for all enterprise providers,
-                    and tools for breaking monolithic applications into
-                    microservices. This plan is tailored for scalability and
-                    integration with lifecycle management tools. For a full
-                    breakdown of its capabilities and to get a personalized
-                    quote,{' '}
+                    Best for organizations seeking to take their platform
+                    engineering to the next level, standardize backend services,
+                    and streamline development processes. This plan includes
+                    features for creating live templates that embed your
+                    organization’s best practices and standards, building
+                    private plugins tailored to your specific needs, and gaining
+                    visibility into all services through a comprehensive service
+                    catalog, all while actively reducing technical debt. It also
+                    offers advanced security features like SSO and 2FA, as well
+                    as premium support.{' '}
                     <Link
                       href={process.env.NEXT_PUBLIC_BOOK_MEETING_URL}
                       passHref={true}
@@ -103,7 +90,7 @@ const PricingFAQ = () => {
                         className="text-secondary-purple"
                         target="_blank"
                       >
-                        contact us directly
+                        Contact us for a personalized quote
                       </a>
                     </Link>
                     .
@@ -114,115 +101,61 @@ const PricingFAQ = () => {
           }
         />
         <Question
-          question="How does Amplication pricing work?"
-          answer={
-            <div>
-              Our pricing plans are applied per each workspace individually. The
-              Free plan is offered for <strong>FREE</strong>. The Essential plan
-              is offered in a monthly or annual subscription, for a single or
-              multiple projects. Enterprise plan pricing will be tailored to
-              your specific requirements.{' '}
-              <Link
-                href={process.env.NEXT_PUBLIC_BOOK_MEETING_URL}
-                passHref={true}
-              >
-                <a
-                  onClick={handleBookDemoClick}
-                  className="text-secondary-purple"
-                  target="_blank"
-                >
-                  Contact us to get a quote
-                </a>
-              </Link>
-              .
-            </div>
-          }
-        />
-        <Question
-          question="Can I upgrade at any time?"
-          answer="Yes! You can upgrade anytime and your workspace plan will be immediately updated."
-        />
-        <Question
-          question="Can I pay monthly?"
-          answer="Sure. We offer monthly and annual plans. The annual plan is offered at a discount."
-        />
-        <Question
-          question="When will I be billed?"
-          answer={
-            <div>
-              Our paid plans are charged upfront and billed on a recurring basis
-              based on your payment schedule preference (monthly or annually).
-            </div>
-          }
-        />
-        <Question
-          question="Does Amplication support advanced security features like 2FA, audit logs, and SSO for organizational security concerns?"
-          answer={
-            <div>
-              Yes, Amplication&apos;s Enterprise plan is equipped with advanced
-              security features to meet the needs of your organization. This
-              includes support for Single Sign-On (SSO), audit logs, and
-              Two-Factor Authentication (2FA).
-            </div>
-          }
-        />
-
-        <Question
-          question="We have multiple teams in the organization, can we use one account to manage separate teams?"
-          answer={
-            <div>
-              Yes, the Enterprise plan lets you have unlimited projects,
-              services, and team members. You can manage all your teams from the
-              same organization in your preferred structure.
-            </div>
-          }
-        />
-        <Question
-          question="Can I pay to Amplication through my AWS account (AWS billing)?"
-          answer={
-            <div>
-              Yes, we support this billing method. Please contact us and we will
-              help you handle it.
-            </div>
-          }
-        />
-        <Question
           question="We have our own best practices and standards - can we ask Amplication to generate the code in our own flavor?"
           answer={
             <div>
-              Amplication offers a structured way to{' '}
-              <a
-                className="text-secondary-purple"
-                target="_blank"
-                rel="noreferrer"
-                href="https://docs.amplication.com/custom-code/"
-              >
-                add custom code
-              </a>{' '}
-              and modify your generated code.
-              <br />
-              We also provide various{' '}
-              <a
-                className="text-secondary-purple"
-                target="_blank"
-                rel="noreferrer"
-                href="https://docs.amplication.com/getting-started/plugins/"
-              >
-                plugins
-              </a>
-              , like Prettier and ESLint, so you can format your code exactly
-              how you need it.
-              <br />
-              Furthermore, you can create{' '}
-              <a
-                className="text-secondary-purple"
-                target="_blank"
-                rel="noreferrer"
-                href="https://docs.amplication.com/enterprise-private-plugins/"
-              >
-                private plugins
-              </a>{' '}
-              that are accessible only within your organization.
+              Absolutely! Amplication allows you to customize code generation to
+              match your organization&apos;s unique best practices and
+              standards. You can use private plugins to extend
+              Amplication&apos;s capabilities, integrating your own tools,
+              workflows, and coding conventions. Additionally, you can create
+              custom live templates that reflect your specific requirements,
+              ensuring that the generated services continuously follow your
+              evolving guidelines. This flexibility ensures that the services
+              generated by Amplication align perfectly with your
+              organization&apos;s flavor and development preferences.
+            </div>
+          }
+        />
+        <Question
+          question="How can Amplication help us maintain our best practices and standards while reducing technical debt?"
+          answer={
+            <div>
+              Amplication streamlines the process of maintaining best practices
+              and standards across your development projects by offering live
+              templates that embed organizational guidelines and are easily
+              updatable. This ensures all services are consistently aligned with
+              the latest standards. Additionally, Amplication provides technical
+              debt monitoring and proactive alerts, helping teams address
+              discrepancies and keep technical debt under control.
+            </div>
+          }
+        />
+        <Question
+          question="Can I pay for Amplication through my AWS account (AWS billing)?"
+          answer={
+            <div>
+              Yes, we support this billing method. Please contact us for
+              assistance.
+            </div>
+          }
+        />
+        <Question
+          question="We are working with the enterprise edition of GitLab / BitBucket, will it work with Amplication?"
+          answer={
+            <div>
+              The Enterprise Plan Supports Bitbucket, AWS CodeCommit, GitLab,
+              Azure DevOps, and any other Git provider.
+            </div>
+          }
+        />
+        <Question
+          question="Can you assist with deploying to various cloud providers like AWS?"
+          answer={
+            <div>
+              Yes, Amplication offers plugins and guides for deployment to
+              services such as Kubernetes, Docker Desktop, and AWS&apos;s ECS.
+              You can also create a custom plugin for other cloud providers.
             </div>
           }
         />
@@ -230,19 +163,18 @@ const PricingFAQ = () => {
           question="How can I customize the generated code, and will the generated code override my changes?"
           answer={
             <div>
-              You can customize the generated code just like any other code
-              changes in your existing code base. Amplication’s Smart Git Sync
-              feature ensures that your custom code always takes precedence over
-              the generated code, so your changes will not be overridden. For
-              more details on how this works, please refer to our{' '}
-              <a
-                className="text-secondary-purple"
-                target="_blank"
-                rel="noreferrer"
-                href="https://docs.amplication.com/smart-git-sync/"
-              >
-                documentation.
-              </a>
+              You can customize the generated code, and Amplication&apos;s Smart
+              Git Sync ensures your custom code takes precedence over the
+              generated code, so your changes won&apos;t be overridden.
+            </div>
+          }
+        />
+        <Question
+          question="Does Amplication support advanced security features like 2FA, audit logs, and SSO for organizational security concerns?"
+          answer={
+            <div>
+              Yes, the Enterprise plan includes support for Single Sign-On
+              (SSO), audit logs, and Two-Factor Authentication (2FA).
             </div>
           }
         />
